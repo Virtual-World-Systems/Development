186dcf322ad62e6bb6974b91de024b07e0fff911
Virtual-World-Systems <virtual-worlds@gmx.net>
2022-12-20 13:37:47 +0100

    initialization

---
+++ Collaborator/OwlEyes/Solution(s)/BrowserControl/BrowserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ernsoft.BrowserControl
+{
+    public partial class BrowserControl: UserControl
+    {
+        public BrowserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
